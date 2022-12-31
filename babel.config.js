6b8fd73fce95860a419891d642042213e5813094
Kelvin Mendes de Araújo <65052645+kelvin-had1@users.noreply.github.com>
2022-12-31 15:21:45 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,7 @@ module.exports = {
           '@modules': './src/modules',
           '@shared': './src/shared'          
         }
-      }],
-      'babel-plugin-transform-typescript-metadata',
+      }]
     ],
     ignore: [
       '**/*.spec.ts'
